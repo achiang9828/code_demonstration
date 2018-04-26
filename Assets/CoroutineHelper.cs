86f41aceea677f961c0dd220d23f1b50740608f5
John <vff9828@gmail.com>
2018-04-27 00:27:03 +0800

    first commit

---
+++ Assets/CoroutineHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CoroutineHelper : MonoBehaviour
+{
+    private static CoroutineHelper _instance = null;
+    public static CoroutineHelper Instance
+    {
+        get
+        { 
+            if (_instance == null)
+            {
+                Debug.LogError ("Please set up instance in your scene.");
+            }
+            return _instance;
+        }
+    }
+
+    void Awake ()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != null)
+        {
+            Destroy (gameObject);
+        }
+        DontDestroyOnLoad (gameObject);
+    }
+
+    public Coroutine Invoke (IEnumerator function)
+    {
+        return StartCoroutine (function);
+    }
+
+    public Coroutine Invoke (float delay, Action function)
+    {
+        return StartCoroutine (RunInvoke (delay, function));
+    }
+
+    public Coroutine InvokeAfterOneFrame (Action function)
+    {
+        return StartCoroutine (RunInvokeAfterOneFrame (function));
+    }
+
+    public void CancelInvoke (Coroutine coroutine)
+    {
+        StopCoroutine (coroutine);
+    }
+
+    IEnumerator RunInvoke (float delay, Action function)
+    {
+        yield return new WaitForSeconds (delay);
+        function.Invoke ();
+    }
+
+    IEnumerator RunInvokeAfterOneFrame (Action function)
+    {
+        yield return null;
+        function.Invoke ();
+    }
+}
---
+++ Assets/CoroutineHelperTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineHelperTest : MonoBehaviour
+{
+    void Start ()
+    {
+        Tester tester = new Tester ();
+        tester.TestInvoke ();
+        tester.TestClock ();
+    }
+}
+
+public class Tester
+{
+    public void TestInvoke ()
+    {
+        Coroutine A = CoroutineHelper.Instance.Invoke (1f, Print);
+        CoroutineHelper.Instance.Invoke (2f, () =>
+        {
+            Debug.LogWarning ("Nice to meet you, too!");
+        });
+    }
+
+    public void TestClock ()
+    {
+        CoroutineHelper.Instance.Invoke (Clock ());
+    }
+
+    void Print ()
+    {
+        Debug.LogWarning ("Nice to meet you!");
+    }
+
+    IEnumerator Clock ()
+    {
+        int timer = 1;
+        while (true)
+        {
+            Debug.Log (string.Format ("time: {0}", timer));
+            timer++;
+            yield return new WaitForSeconds (1);
+        }
+    }
+}
